cdcb54e424cab9503ba3a05048a6993a23525176
banflam <rishiagrawal2002@gmail.com>
2025-07-31 11:13:26 -0400

    rewrite the random function for the random quote

--- script.js
+++ script.js
@@ -228,18 +228,35 @@ const quotes = [
   },
 ];
 
-const quote_text = document.querySelector(".quote_text");
-const author_text = document.querySelector(".author_text");
+const quote_text_main = document.getElementById("quote_text_main");
+const author_text_main = document.getElementById("author_text_main");
+
+const quote_text_footer = document.getElementById("quote_text_footer");
+const author_text_footer = document.getElementById("author_text_footer");
+
 const random_link = document.querySelector(".random");
 
-console.log(quote_text, author_text, random_link);
+console.log(quote_text_main, author_text_main, random_link);
 
-const set_random_quote = function () {
-  var random_quote = quotes[Math.floor(Math.random() * quotes.length)];
-  quote_text.textContent = random_quote.text;
-  author_text.textContent = random_quote.author;
+const set_random_quotes = function () {
+  var main_quote_idx = Math.floor(Math.random() * quotes.length);
+  var footer_quote_idx = Math.floor(Math.random() * quotes.length);
+
+  while (main_quote_idx === footer_quote_idx) {
+    footer_quote_idx = Math.floor(Math.floor(Math.random() * quotes.length));
+  }
+
+  var quote_main = quotes[main_quote_idx];
+  var quote_footer = quotes[footer_quote_idx];
+
+  quote_text_main.textContent = quote_main.text;
+  author_text_main.textContent = quote_main.author;
+
+  quote_text_footer.textContent = quote_footer.text;
+  author_text_footer.textContent = quote_footer.author;
 };
+
 random_link.addEventListener("click", function (e) {
   e.preventDefault();
-  set_random_quote();
+  set_random_quotes();
 });
